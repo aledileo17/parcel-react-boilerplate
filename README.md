8c8f8248cd176d1cd835a8c3d4c45f6420125f20
Alejandro Di Leo <ale.dileo17@gmail.com>
2018-10-13 17:52:46 -0300

    Created minimal boilerplate

---
+++ README.md
@@ -0,0 +1,18 @@
+# Minimal React Boilerplate
+
+## Specs
+
+- Parcel
+- React 16.5.2
+
+## Requirements
+
+- Node 8 or higher
+
+## Installation
+
+```sh
+git clone https://github.com/aledileo17/parcel-react-boilerplate
+cd parcel-react-boilerplate
+npm i && npm start
+```
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from "react";
+import "./index.scss";
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>Minimal React Boilerplate with Parcel</h1>
+      </Fragment>
+    );
+  }
+}
+
+export default App;
